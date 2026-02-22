18d151f4aefc2990462c58a29e262944e6302a50
Dhinesh Kumar <sdhineshkumar2005@gmail.com>
2026-02-22 12:42:27 +0530

    Product Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import Checkout from './pages/Checkout';
-import Home  from './pages/Home';
+import Home from './pages/Home';
 import Auth from './pages/Auth';
 import Navbar from './components/Navbar';
 import AuthProvider from './context/AuthContext';
+import ProductDetails from './pages/ProductDetails';
 
 
 function App() {
   return (
     <AuthProvider>
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth/>}/>
-        <Route path="/checkout" element={<Checkout />}/>
-      </Routes>
-    </div>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
     </AuthProvider>
   )
 }
